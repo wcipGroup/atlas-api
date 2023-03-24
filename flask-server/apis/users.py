00fb08507631cc3b9a62b1a22b444cd492c18edc
paulBooz <82902650+paulBooz@users.noreply.github.com>
2023-03-24 12:35:18 +0200

    Update users.py

--- flask-server/apis/users.py
+++ flask-server/apis/users.py
@@ -130,8 +130,6 @@ def getPredictions(devAddr):
     db = Mongo.get_db()
     col = db["predictions"]
     sensor_data = list(col.find())
-    if len(sensor_data):
-        sensor_data = sensor_data[0]
     return jsonify(data=sensor_data), 200
 
 
